3a337acc21b79f145dd0846d0c394fe89f0cfbd3
Shrey85 <157231201+Shrey85@users.noreply.github.com>
2024-02-26 13:35:14 -0500

    my-function.js

--- my-function.js
+++ my-function.js
@@ -1,5 +1,5 @@
 console.log('loading function');
 export const handler = async(event,context) => {
   console.log('Event logged from Lambda: ', event);
-  return  event.name+ ' says '+  event.keyword+ '!'; // Echo back the name
+  return   'Shresta  says '+  event.keyword+ '!'; // Echo back the name
 };
