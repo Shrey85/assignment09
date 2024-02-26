6ab02f1f0e585b869ef0320ac406731f069f87d3
shresta <sbalara2@uncc.edu>
2024-02-25 21:39:11 -0500

    my-function

---
+++ my-function.js
@@ -0,0 +1,5 @@
+console.log('loading function');
+export const handler = async(event,context) => {
+  console.log('Event logged from Lambda: ', event);
+  return  event.name+ ' says '+  event.keyword+ '!'; // Echo back the name
+};
